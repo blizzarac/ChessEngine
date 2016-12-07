5f1bad1d3f890b1f1f7f4360180eabe6e4963f7f
Alexander Stolz <alexanderstolz@Alexanders-MacBook-Pro.local>
2016-12-07 16:24:19 +0100

    initial

---
+++ src/com/stolz/alexander/chessengine/pieces/Empty.java
@@ -0,0 +1,43 @@
+package com.stolz.alexander.chessengine.pieces;
+
+import com.stolz.alexander.chessengine.pieces.Piece;
+
+public class Empty extends Piece {
+	private int i;
+	private int j;
+	private String name = "com.stolz.alexander.chessengine.pieces.Empty";
+
+	public Empty(int type, int ii, int jj) {
+		super(type);
+		i = ii;
+		j = jj;
+	}
+	
+	@Override
+	public int icoord(){
+		if(i > 8){
+		return 7;}
+		
+		if(i < 0){
+		return 0;}
+		
+		else{return i;}
+	}
+	
+	@Override
+	public int jcoord(){
+		if(j > 8){
+		return 7;}
+		
+		if(j < 0){
+		return 0;}
+		
+		else{return j;}
+	}
+	
+	@Override
+	public String toString(){
+		return name;
+	}
+
+}
