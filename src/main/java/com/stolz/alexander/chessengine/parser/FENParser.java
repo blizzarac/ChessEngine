fd08ce5a06092dbe0f13460b7c0280cb7dc67d24
blizzarac <stolz.alexander@gmail.com>
2017-01-04 21:33:27 +0100

    FEN Parser work and tests

--- src/main/java/com/stolz/alexander/chessengine/parser/FENParser.java
+++ src/main/java/com/stolz/alexander/chessengine/parser/FENParser.java
@@ -4,6 +4,10 @@ import com.stolz.alexander.chessengine.engine.logic.ChessBoard;
 import com.stolz.alexander.chessengine.engine.pieces.*;
 
 import java.net.Inet4Address;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import static com.stolz.alexander.chessengine.engine.pieces.PieceColor.BLACK;
 
 /**
  * Created by astolz on 03.01.2017.
@@ -31,7 +35,6 @@ public class FENParser {
                 Piece createdPiece = symToPiece(pos);
                 if (createdPiece != null) {
                     board.pieces[col][row] = createdPiece;
-                    System.out.print(board.printBoard() + "\n");
                 }
 
                 col += 1;
@@ -41,7 +44,14 @@ public class FENParser {
             col = 0;
         }
 
+        parseGameInformation(parts[7].substring(9), board);
+
         return board;
+    }
+
+    private void parseGameInformation(String info, ChessBoard board) {
+        String [] infoParts = info.split(" ");
+        board.currentPlayer = infoParts[0].equals("w")?PieceColor.WHITE:PieceColor.BLACK;
     }
 
     private Piece symToPiece(char sym) {
@@ -71,8 +81,58 @@ public class FENParser {
         return result;
     }
 
+    @FunctionalInterface
+    interface Function2 <A, B, R> {
+        //R is like Return, but doesn't have to be last in the list nor named R.
+        R apply (A a, B b);
+    }
+
     public String export(ChessBoard board) {
-        return null;
+        Piece [][] pieces = board.pieces;
+        StringBuilder builder = new StringBuilder();
+        int emptyCounter = 0;
+
+        Function2<Piece, String, String> blackWhite =
+                (Piece piece, String sym) -> piece.getColor()==BLACK?sym.toLowerCase():sym;
+
+        final Consumer<Integer> emptyCheck = (Integer counter) -> {if(counter>0){builder.append(counter);}};
+
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                if (pieces[y][x].getType() == PieceType.NOTYPE) {
+                    emptyCounter++;
+                } else if (pieces[y][x].getType() == PieceType.PAWN) {
+                    emptyCheck.accept(emptyCounter);
+                    emptyCounter = 0;
+                    builder.append(blackWhite.apply(pieces[y][x], "P"));
+                } else if (pieces[y][x].getType() == PieceType.KING) {
+                    emptyCheck.accept(emptyCounter);
+                    emptyCounter = 0;
+                    builder.append(blackWhite.apply(pieces[y][x], "K"));
+                } else if (pieces[y][x].getType() == PieceType.QUEEN) {
+                    emptyCheck.accept(emptyCounter);
+                    emptyCounter = 0;
+                    builder.append(blackWhite.apply(pieces[y][x], "Q"));
+                } else if (pieces[y][x].getType() == PieceType.BISHOP) {
+                    emptyCheck.accept(emptyCounter);
+                    emptyCounter = 0;
+                    builder.append(blackWhite.apply(pieces[y][x], "B"));
+                } else if (pieces[y][x].getType() == PieceType.KNIGHT) {
+                    emptyCheck.accept(emptyCounter);
+                    emptyCounter = 0;
+                    builder.append(blackWhite.apply(pieces[y][x], "N"));
+                } else if (pieces[y][x].getType() == PieceType.ROOK) {
+                    emptyCheck.accept(emptyCounter);
+                    emptyCounter = 0;
+                    builder.append(blackWhite.apply(pieces[y][x], "R"));
+                }
+            }
+            emptyCheck.accept(emptyCounter);
+            emptyCounter = 0;
+            if (x < 7) builder.append("/");
+        }
+
+        return builder.toString();
     }
 
 }
--- src/test/java/com/stolz/alexander/chessengine/engine/parser/FENParserTest.java
+++ src/test/java/com/stolz/alexander/chessengine/engine/parser/FENParserTest.java
@@ -1,6 +1,7 @@
 package com.stolz.alexander.chessengine.engine.parser;
 
 import com.stolz.alexander.chessengine.engine.logic.ChessBoard;
+import com.stolz.alexander.chessengine.engine.pieces.PieceColor;
 import com.stolz.alexander.chessengine.parser.FENParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -60,4 +61,121 @@ public class FENParserTest {
                         "RNBQKBNR\n",
                 result.printBoard());
     }
+
+    @Test
+    @DisplayName("should create board from FEN with white to move")
+    void test2() {
+        // Given
+        final FENParser sut = new FENParser();
+        final String startFAN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+        // When
+        final ChessBoard result = sut.parse(startFAN);
+
+        // Then
+        Assertions.assertEquals(result.currentPlayer, PieceColor.WHITE);
+    }
+
+    @Test
+    @DisplayName("should create board from FEN with black to move")
+    void test3() {
+        // Given
+        final FENParser sut = new FENParser();
+        final String startFAN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1";
+
+        // When
+        final ChessBoard result = sut.parse(startFAN);
+
+        // Then
+        Assertions.assertEquals(result.currentPlayer, PieceColor.BLACK);
+    }
+
+    @Test
+    @DisplayName("should create board from FEN with correct Castling availability")
+    void test4() {
+        // Given
+        final FENParser sut = new FENParser();
+        final String startFAN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1";
+
+        // When
+        final ChessBoard result = sut.parse(startFAN);
+
+        // Then
+        Assertions.assertTrue(false);
+    }
+
+    @Test
+    @DisplayName("should create board from FEN with correct En passant")
+    void test5() {
+        // Given
+        final FENParser sut = new FENParser();
+        final String startFAN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1";
+
+        // When
+        final ChessBoard result = sut.parse(startFAN);
+
+        // Then
+        Assertions.assertTrue(false);
+    }
+
+    @Test
+    @DisplayName("should create board from FEN with correct Halfmove clock")
+    void test6() {
+        // Given
+        final FENParser sut = new FENParser();
+        final String startFAN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1";
+
+        // When
+        final ChessBoard result = sut.parse(startFAN);
+
+        // Then
+        Assertions.assertTrue(false);
+    }
+
+    @Test
+    @DisplayName("should create board from FEN with correct Fullmove clock")
+    void test7() {
+        // Given
+        final FENParser sut = new FENParser();
+        final String startFAN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1";
+
+        // When
+        final ChessBoard result = sut.parse(startFAN);
+
+        // Then
+        Assertions.assertTrue(false);
+    }
+
+    @Test
+    @DisplayName("should produce correct FEN from Board for starting Position")
+    void test8() {
+        // Given
+        final FENParser sut = new FENParser();
+        ChessBoard board = new ChessBoard();
+        board.init();
+        final String startFAN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+        // When
+        final String result = sut.export(board);
+
+        // Then
+        Assertions.assertTrue(startFAN.contains(result));
+    }
+
+    @Test
+    @DisplayName("should produce correct FEN from Board for non starting Position")
+    void test9() {
+        // Given
+        final FENParser sut = new FENParser();
+        final String startFAN = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
+        ChessBoard board = sut.parse(startFAN);
+
+        // When
+        final String result = sut.export(board);
+
+        // Then
+        Assertions.assertTrue(startFAN.contains(result));
+    }
+
+
 }
